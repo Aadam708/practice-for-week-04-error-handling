19f9fb6de425d46da5a978f81ee5ed6f59b99c69
Aadam Ali <aadamali708@gmail.com>
2024-02-25 12:21:57 +0000

    finished error checks

--- error-handling.js
+++ error-handling.js
@@ -1,26 +1,62 @@
 // 1.
 function sum(array) {
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
+  try{
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum;
   }
-  return sum;
+  catch(error){
+    console.error('not an array type so a type error')
+  }
 }
 
 let res = sum(null);
+
+
 console.log(res);
 
 // 2.
 // tests
+
+/**
+ * Prints the given name.
+ * @param {string} name - The name to be printed.
+ */
+const sayName = name =>{
+
+  try{
+    if(typeof name !==  'string') throw Error('name is not a string')
+    else console.log(name)
+  }
+
+  catch(error){
+    console.log(error.message)
+  }
+
+}
+
 sayName("Alex");
 sayName(1);
 // Your code here
 
+
+
+
 // 3.
 function greet(greeting) {
-  if (!greeting) {
-    throw new Error("There was no greeting given.");
+  try{
+    if (greeting.includes('loser')) {
+      throw new Error("There was no greeting given you bad boy being rude.");
+    }
+    console.log(greeting);
+
+  }
+  catch(error){
+    console.error("Error: "+error.message);
   }
 
-  console.log(greeting);
 }
+greet("hello bob u loser");
+greet('hi dave im your friend catch my pokemon xyz pls')
